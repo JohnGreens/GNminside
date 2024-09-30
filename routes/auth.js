812b04e112820ae08db8b13985fd0a7488e3f6af
EGimre <gimre85@hotmail.com>
2024-09-30 22:01:33 +0200

    feat: fileter id opprettet for google og linkedin, men mangler nytt api endepunkt, demo lagt inn

--- routes/auth.js
+++ routes/auth.js
@@ -8,12 +8,71 @@ const pca = require('../config/passport-setup');
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
-    res.redirect('/');
+    const user = {
+        id: req.user.id,  
+        provider: 'Google'
+    };
+
+    // Post to your API
+    axios.post('https://prod-08.northcentralus.logic.azure.com:443/workflows/a866db458a254ce38fb746a844b1fb91/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vOVP3IY0qM4C0o5lHIC_7_pkoE7X78gF5EJ5uLwaW98', {
+        TennentID: "fb174ad5-c5b5-452d-8240-34675b784ba5",
+        TypeOfRequest: "GetMinsideIDs"  
+    })
+    .then(apiResponse => {
+        // console.log(apiResponse)
+        if (apiResponse.data && apiResponse.data.eaas) {
+            console.log(apiResponse.data.eaas[1])
+            // If user is found, log them in and proceed
+            user.eaasID=apiResponse.data.eaas
+            user.klimaID=apiResponse.data.klima
+            req.login(user, function(err) {
+                if (err) { return next(err); }
+                return res.redirect('/');  // Redirect to home or dashboard
+            });
+        } else {
+            // If no user is found, redirect to registration page
+            res.redirect('/register');
+        }
+    })
+    .catch(error => {
+        console.error("Error posting to API:", error);
+        res.status(500).send('Authorization failed');
+    });
 });
 
 router.get('/linkedin', passport.authenticate('linkedin'));
+
 router.get('/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/login' }), (req, res) => {
-    res.redirect('/');
+    const user = {
+        id: req.user.id,  
+        provider: 'linkedin'
+    };
+
+    // Post to your API
+    axios.post('https://prod-08.northcentralus.logic.azure.com:443/workflows/a866db458a254ce38fb746a844b1fb91/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vOVP3IY0qM4C0o5lHIC_7_pkoE7X78gF5EJ5uLwaW98', {
+        TennentID: "fb174ad5-c5b5-452d-8240-34675b784ba5",
+        TypeOfRequest: "GetMinsideIDs"  
+    })
+    .then(apiResponse => {
+        // console.log(apiResponse)
+        if (apiResponse.data && apiResponse.data.eaas) {
+            console.log(apiResponse.data.eaas[1])
+            // If user is found, log them in and proceed
+            user.eaasID=apiResponse.data.eaas
+            user.klimaID=apiResponse.data.klima
+            req.login(user, function(err) {
+                if (err) { return next(err); }
+                return res.redirect('/');  // Redirect to home or dashboard
+            });
+        } else {
+            // If no user is found, redirect to registration page
+            res.redirect('/register');
+        }
+    })
+    .catch(error => {
+        console.error("Error posting to API:", error);
+        res.status(500).send('Authorization failed');
+    });
 });
 
 
@@ -36,31 +95,6 @@ router.get('/azuread', (req, res) => {
         });
 });
 
-// Azure AD callback route
-// router.get('/azuread/callback', (req, res, next) => {
-//     const tokenRequest = {
-//         code: req.query.code,
-//         scopes: ["user.read"],
-//         redirectUri: 'http://localhost:3000/auth/azuread/callback',
-//     };
-
-//     pca.acquireTokenByCode(tokenRequest)
-//         .then(response => {
-//             const user = {
-//                 id: response.account.tenantId,
-//                 provider: 'Microsoft'
-//             };
-
-//             req.login(user, err => {
-//                 if (err) { return next(err); }
-//                 res.redirect('/'); // Redirect to the home page or dashboard
-//             });
-//         })
-//         .catch(error => {
-//             console.error(error);
-//             res.status(500).send('Authentication failed');
-//         });
-// });
 router.get('/azuread/callback', (req, res, next) => {
     const tokenRequest = {
         code: req.query.code,
