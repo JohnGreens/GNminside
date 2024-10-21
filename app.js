a1cc947e0bb9a6fe51a47b0a829cf101e69a8333
EGimre <gimre85@hotmail.com>
2024-10-21 13:16:35 +0200

    Feat: Laste opp til azure

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-    secret: 'your_secret_key',
+    secret: 'espenErBest,Litt selvgod må man være',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 }
+    cookie: { secure: true, maxAge: 24 * 60 * 60 * 1000 }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
